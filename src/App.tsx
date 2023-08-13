f5f5920bbf4ef6a2d0419b7c42b227a1441e187c
Alexander <evening.sasha@mail.ru>
2023-08-13 22:35:56 +0300

    grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
 import React from 'react';
 import './App.css';
-import Technologies from './Technologies'
-import Header from './Header'
-import Footer from './Footer'
 const App = () => {
   return (
     <div>
-        <Header/>;
-        <Technologies/>;
-        <Footer/>;
+
     </div>)
 }
 
