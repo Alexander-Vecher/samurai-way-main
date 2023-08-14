211dfdf7710aad596d44694824cd8b8368eca8b7
Alexander <evening.sasha@mail.ru>
2023-08-14 23:08:29 +0300

    grid.

--- src/Footer.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Footer = () => {
-    return (<a href = '#s'>
-        Hello, samurai! Let's go!
-    </a>)}
-export default Footer;
--- src/Header.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const Header = () => {
-    return (<a href = '#s'>
-        Hello, samurai! Let's go!
-    </a>)}
-export default Header;
-
