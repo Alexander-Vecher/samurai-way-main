01c80999d90b121bd5661e50f3e62e79ec6b9a5b
Alexander <evening.sasha@mail.ru>
2023-08-26 23:39:37 +0300

    components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import './App.css';
+import {Header} from './components/Header'
+import {Navbar} from './components/Navbar'
+import {Profile} from "./components/Profile";
+
 const App = () => {
-  return (
-    <div>
+    return (
+        <div>
 
-    </div>)
+        <Header/>
+             <Navbar/>
+            <Profile/>
+</div>
+)
 }
-
 
 
 export default App;
