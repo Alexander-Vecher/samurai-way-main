5839a1b4af104141f650aa6d13fb5e4c675912ac
Alexander <evening.sasha@mail.ru>
2023-08-27 23:10:16 +0300

    props

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import {Header} from './components/Header'
 import {Navbar} from './components/Navbar'
 import {Profile} from "./components/Profile";
+import {MyPosts} from "./components/MyPosts";
 
 const App = () => {
     return (
@@ -11,6 +12,7 @@ const App = () => {
         <Header/>
              <Navbar/>
             <Profile/>
+            <MyPosts/>
 </div>
 )
 }
