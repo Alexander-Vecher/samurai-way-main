d7e649d390178946575f43631f2eef927766c14f
Alexander <evening.sasha@mail.ru>
2023-08-27 23:08:07 +0300

    components

---
+++ src/components/MyPosts.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import {Post} from './Post/Post';
+
+export const MyPosts = ()=>{
+    return (
+        <div>
+            My posts
+            <div>
+                <textarea></textarea>
+                <button>Add post</button>
+        </div>
+            <div className={s.posts}>
+                <Post message='Hi, how are you ?'
+                <Post message='It's my first...
+                      </div>
+                      </div>
+    )
+}
---
+++ src/components/Post/Posts.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+export const Post = (props)=> {
+    return (
+        <div className={s.item}>
+            <div>
+                <span>like</span>
+            </div>
+        </div>
+    )
+}
